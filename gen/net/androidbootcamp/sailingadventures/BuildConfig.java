2007a9d5925c71b9b802966fb8a220949c810fa4
Jared Bridgham <k3b910@tds.net>
2015-03-30 20:06:18 -0400

    Chapter 8 commit

---
+++ gen/net/androidbootcamp/sailingadventures/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.androidbootcamp.sailingadventures;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
